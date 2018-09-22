3bb75f5b14f6aade845fecd31575d7d60f956365
ahmadbaal <asyraf13@hotmail.com>
2018-09-22 07:17:07 +0700

    initial commit tutorial2

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,78 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+	@RequestMapping("/viral")
+	public String index() {
+		return "viral";
+	}
+	
+	@RequestMapping("/challenge")
+	public String challenge(@RequestParam(value = "name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "challenge";
+	}
+	
+	@RequestMapping(value = {"/challenge", "challenge/{name}"})
+	public String challengePath(@PathVariable Optional<String> name, Model model) {
+		if (name.isPresent()) {
+			model.addAttribute("name", name.get());
+		}
+		else {
+			model.addAttribute("name", "KB");
+		}
+		return "challenge";
+	}
+	
+	//latihan viral generator
+	@RequestMapping("/generator")
+	public String generator(@RequestParam(value="a", defaultValue = "0") int a,
+							@RequestParam(value="b", defaultValue = "0") int b, Model model) {
+		
+		//menambahkan value ke model
+		model.addAttribute("a", a);
+		model.addAttribute("b", b);
+		
+		//var string hasil
+		String hasil = "";
+		
+		//ubah a dan b jadi 1
+		if (a == 0) {
+			a = 1;
+		}
+		if (b == 0) {
+			b = 1;
+		}
+		
+		//hm loop
+		for (int i = 0; i < b; i++) {
+			if (i > 0 && i < b) {
+						
+				//menambahkan spasi untuk setiap hm
+				hasil += " ";
+				}
+				hasil += "h";
+					
+			if (a == 0) {
+				hasil += "m";
+				}
+					
+			for (int j = 0; j < a; j++) {
+				hasil += "m";
+				}
+		}
+		
+		//menambahkan value ke result
+		model.addAttribute("hasil", hasil);
+		
+		return "generator";
+	}
+}
